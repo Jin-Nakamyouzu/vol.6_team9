63baf4f7ebdefea681272a98cb9652b1052de094
wata616d65 <ok230151@oks.iput.ac.jp>
2025-08-27 22:01:45 +0900

    StartScreenのCSSの変更

--- team9/src/screen/startScreen.tsx
+++ team9/src/screen/startScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// startScreen.css をインポート
+import '../screen/startScreen.css'; // stylesディレクトリにcssを移動した場合
 
 const StartScreen: React.FC = () => {
   return (
-    <div className="screen-container">
+    <div className="start-screen-container"> {/* クラス名を固有のものに変更 */}
       <h1 className="title">StyleBender</h1>
       <div className="button-container">
-        <Link to="/card-view" className="button-link">
+        <Link to="/card-view" className="nav-button"> {/* classNameをnav-buttonに統一 */}
           カード選択へ
         </Link>
-        <Link to="/stage-select" className="button-link">
+        <Link to="/stage-select" className="nav-button"> {/* classNameをnav-buttonに統一 */}
           ステージ選択へ
         </Link>
       </div>
