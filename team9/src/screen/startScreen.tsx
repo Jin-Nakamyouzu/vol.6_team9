386f091475f550008dc892c07c4d07fd6439fbfe
Jin-Nakamyouzu <3n3.nazu3.Naka@gmail.com>
2025-08-28 11:19:26 +0900

    画面遷移を出来るように

--- team9/src/screen/startScreen.tsx
+++ team9/src/screen/startScreen.tsx
@@ -1,37 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routers, Route } from "react-router-dom";
-import "../App.css";
+import { Link } from "react-router-dom";
+import "./cssByScreen/startScreen.css";
 
 const StartScreen: React.FC = () => {
     return (
-        <div className="screen-container">
+        <>
             <h1 className="title">Style Bender</h1>
-            <BrowserRouter className="operation">
-                <Routers>
-                    {/* <div class="content">
-                    <button class="start" Link to="/">スタート</button>
-                    <p class="start-hover-text">
-                        スタートボタンを押すとゲームが始まります
+            <div className="operation">
+                <Link to="./viewCards.tsx" classNameName="content">
+                    <button className="card">カード</button>
+                    <p className="card-hover-text">
+                        カード一覧を見ることができます
                     </p>
-                </div> */}
-                    <Route className="content" path="/" element="{viewCards /}">
-                        <button className="card">カード</button>
-                        <p className="card-hover-text">
-                            カード一覧を見ることができます
-                        </p>
-                    </Route>
-                    <Route
-                        className="content"
-                        path="/"
-                        element="{selectStages /}">
-                        <button className="stage">ステージ</button>
-                        <p className="stage-hover-text">
-                            ステージ一覧を見ることができます
-                        </p>
-                    </Route>
-                </Routers>
-            </BrowserRouter>
-        </div>
+                </Link>
+                <Link to="./selectStage.tsx" className="content">
+                    <button className="card">ステージ</button>
+                    <p className="card-hover-text">
+                        ステージ一覧を見ることができます
+                    </p>
+                </Link>
+            </div>
+        </>
     );
 };
 
