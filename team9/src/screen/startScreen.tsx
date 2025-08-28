69a24e23f17b70ee2132d80a456e259b149678e8
Jin-Nakamyouzu <3n3.nazu3.Naka@gmail.com>
2025-08-28 10:28:30 +0900

    start画面の背景とボタン配置

--- team9/src/screen/startScreen.tsx
+++ team9/src/screen/startScreen.tsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Routers, Route } from "react-router-dom";
 import "../App.css";
 
 const StartScreen: React.FC = () => {
     return (
         <div className="screen-container">
-            <h1 className="title">StyleBender</h1>
-            <div className="button-container">
-                <Link to="/card-view" className="button-link">
-                    カード選択へ
-                </Link>
-                <Link to="/stage-select" className="button-link">
-                    ステージ選択へ
-                </Link>
-            </div>
+            <h1 className="title">Style Bender</h1>
+            <BrowserRouter className="operation">
+                <Routers>
+                    {/* <div class="content">
+                    <button class="start" Link to="/">スタート</button>
+                    <p class="start-hover-text">
+                        スタートボタンを押すとゲームが始まります
+                    </p>
+                </div> */}
+                    <Route className="content" path="/" element="{viewCards /}">
+                        <button className="card">カード</button>
+                        <p className="card-hover-text">
+                            カード一覧を見ることができます
+                        </p>
+                    </Route>
+                    <Route
+                        className="content"
+                        path="/"
+                        element="{selectStages /}">
+                        <button className="stage">ステージ</button>
+                        <p className="stage-hover-text">
+                            ステージ一覧を見ることができます
+                        </p>
+                    </Route>
+                </Routers>
+            </BrowserRouter>
         </div>
     );
 };
